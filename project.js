1a1133a39a6bef08a20a37b9f4862ae67734eddb
michidevall <85964463+michidevall@users.noreply.github.com>
2021-08-30 15:45:04 -0600

    added function

--- project.js
+++ project.js
@@ -1,5 +1,9 @@
 function getGreeting(name){
     return "Hi " + name;
 }
+function add (num1,num2) {
+    return num1 + num2;
+}
 
-module.exports = getGreeting;
+
+module.exports = { getGreeting, add }
--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-const getGreeting = require('./project');
+const { getGreeting, add } = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
+})
+
+// test('add num1, num2', () => {
+//   expect(getSum(5,2)).toBe(7);
+// });
+ test('Return sum of parameter', () => {
+  expect(add(5,2)).toEqual(7);
 });
